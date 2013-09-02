7956d3999b48fee4336edc222e678cea77482ccd
MakarovAlexey <alexeys9@yandex.ru>
2013-09-02 18:14:16 +0600

    Session implementation

--- session.lisp
+++ session.lisp
@@ -1,59 +1,62 @@
 (in-package #:cl-db)
 
 (defvar *default-database-interface*)
+
 (defvar *default-connection-args*)
+
 (defvar *default-mapping-schema*)
-
-(defmacro with-session ((&rest options) &body body)
-  `(call-with-session
-    (quote ,@(compile-option :database-interface options))
-    (quote ,@(compile-option :mapping-schema options))
-    (list ,@(compile-option :connection-args options))
-    #'(lambda () ,@body)))
-
-(defclass mapping-configuration ()
-  ((mapping-schema :initarg :mapping-schema
-		   :reader mapping-schema-of)
-   (open-connection :initarg :open-connection-function
-		    :reader open-connection-function-of)
-   (close-connection :initarg :close-connection-function
-		     :reader close-connection-function-of)
-   (prepare-statement :initarg :prepare-statement-function
-		      :reader prepare-statement-function-of)
-   (execute-statement :initarg :execute-statement-function
-		      :reader execute-statement-function-of)
-   (list-metadata :initarg :list-metadata-function
-		  :reader list-metadata-function-of)))
-
-(defun open-connection (configuration &rest args)
-  (apply (open-connection-function-of configuration) args))
-
-(defun close-connection (configuration)
-  (funcall (close-connection-function-of configuration)))
-
-(defclass clos-session ()
-  ((mappings :initarg :mappings
-	     :reader mappings-of)
-   (connection :initarg :connection
-	       :reader connection-of)
-   (loaded-objects :initarg :loaded-objects
-		   :reader loaded-objects-of)
-   (prepared-statement-counter :initform 0
-			       :accessor prepared-statement-counter-of)))
-
-(defun prepare-statement (session query)
-  (funcall
-   (prepare-statement-function-of
-    (configuration-of session))
-   (connection-of session)
-   (format nil "prepaired_statement_~a"
-	   (incf (prepared-statement-counter-of session)))
-   query))
 
 (defvar *session*)
 
-(defun call-with-session (session function)
-  (let ((*session* session))
-    (funcall function)))
+(defclass clos-session ()
+  ((database-interface :initarg :database-interface
+		       :reader database-interface-of)
+   (mapping-schema :initarg :mapping-schema
+		   :reader mapping-schema-of)
+   (connection :initarg :connection
+	       :reader connection-of)
+   (loaded-objects :initarg :loaded-objects
+		   :reader loaded-objects-of)))
 
+(defun open-session (mapping-schema
+		     database-interface &rest connection-args)
+  (make-instance 'clos-session
+		 :mapping-schema mapping-schema
+		 :databse-interface database-interface
+		 :connection (apply #'open-connection
+				    database-interface
+				    connection-args)))
+
+(defun close-session (session)
+  (close-connection session))
+
+(defun call-with-session (function database-interface-name
+			  mapping-schema-name &rest connection-args)
+  (let* ((*session*
+	  (apply #'open-session
+		 (ensure-mapping-schema mapping-schema-name)
+		 (get-database-interface database-interface-name)
+		 connection-args)))
+    (funcall function)
+    (close-session *session*)))
+
+(defmacro with-session ((&rest options) &body body)
+  `(apply #'call-with-session
+	  #'(lambda () ,@body)
+	  (quote ,(or (first
+		       (compile-option :database-interface options))
+		      *default-database-interface*))
+	  (quote ,(or (first
+		       (compile-option :mapping-schema options))
+		      *default-mapping-schema*))
+	  ,(or (compile-option :connection-args options)
+	       *default-connection-args*)))
+
+;; 1. надо проверять открытие и закрытие соединения
+
+;; 2. продумать: сессия только открывает соединение и собирает объекты
+;; транзакции (единицы работы (unit of work)) являются контекстом
+;; работы с объектами т.е. вне транзации не производится отслеживания
+;; состояния объектов, но можно их загружать (stateless session в
+;; nhibernate)
 
