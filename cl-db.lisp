954ed6561005b0263acadaef0e113075649fd039
Makarov Alexey <alexeys9@yandex.ru>
2014-07-05 11:52:50 +0600

    implementation

--- cl-db.lisp
+++ cl-db.lisp
@@ -34,36 +34,85 @@
 (defun compute-inheritance (class-mapping root-name &rest class-mappings)
   (parse-mapping #'(lambda (&key class-name table-name primary-key
 			      superclasses &allow-other-keys)
-		     (list* class-name
-			    table-name 
-			    (mapcar #'(lambda (superclass-mapping)
-					(destructuring-bind (class-name &rest columns)
-					    superclass-mapping
-					  (list*
-					   (apply #'compute-inheritance
-						  (assoc class-name class-mappings)
-						  class-mappings)
-					   columns)))
-				    (remove root-name superclasses :key #'first))))
+		     (list :class-name class-name
+			   :table-name table-name
+			   :primary-key primary-key
+			   :superclasses
+			   (mapcar #'(lambda (superclass-mapping)
+				       (destructuring-bind (class-name &rest columns)
+					   superclass-mapping
+					 (list*
+					  (apply #'compute-inheritance
+						 (assoc class-name class-mappings)
+						 class-mappings)
+					  columns)))
+				   (remove root-name superclasses :key #'first))))
 		 class-mapping))
 
-(defun compute-mapping (class-mapping root-name &rest class-mappings)
-  (parse-mapping #'(lambda (&key class-name primary-key &allow-other-keys)
-		     (append
-		      (apply #'compute-inheritance class-mapping
-			     root-name class-mappings)
-		      (apply #'compute-subclass-mappings class-name
-			     primary-key class-mappings)))
-		 class-mapping))
+
+
+(defun compute-class-mappings (class-mapping &rest class-mappings)
+  (destructuring-bind
+	(class-name ((table-name &rest primary-key)
+		     &rest superclasses)
+		    &rest slot-mappings) class-mapping
+    (list* (list class-name table-name primary-key)
+	   (when (not (null class-mappings))
+	     (apply #'compute-class-mappings class-mappings)))))
+
+(defun compute-superclass-mappings (class-mapping &rest class-mappings)
+  (destructuring-bind
+	(class-name ((table-name &rest primary-key)
+		     &rest superclasses)
+		    &rest slot-mappings) class-mapping
+    (append
+     (reduce #'(lambda (superclass-mappings superclass-mapping)
+		 (destructuring-bind (superclass-name &rest foreign-key)
+		     superclass-mapping
+		   (list*
+		    (list class-name superclass-name foreign-key)
+		    superclass-mappings)))
+	     superclasses
+	     :initial-value nil)
+     (when (not (null class-mappings))
+       (apply #'compute-superclass-mappings class-mappings)))))
+
+(defun compute-value-mappings (class-mapping &rest class-mappings)
+  (list*
+   (destructuring-bind
+	 (class-name ((table-name &rest primary-key)
+		      &rest superclasses)
+		     &rest slot-mappings) class-mapping
+     (list class-name 
+	   (reduce #'(lambda (slot-mappings slot-mapping)
+		       (destructuring-bind
+			     (slot-name (mapping-type &rest args)
+					&optional deserializer serializer)
+			   slot-mapping
+			 (if (eq mapping-type :property)
+			     (list*
+			      (list slot-name args deserializer serializer)
+			      slot-mappings)
+			     slot-mappings)))
+		   slot-mappings
+		   :initial-value nil)))
+   (when (not (null class-mappings))
+     (apply #'compute-value-mappings class-mappings))))
 
 (defmacro define-schema (name params &body class-mappings)
   (declare (ignore params))
   `(defun ,name ()
-     (list
-      (quote ,(mapcar #'(lambda (class-mapping)
-			   (apply #'compute-mapping
-				  class-mapping nil class-mappings))
-		       class-mappings)))))
+     (list :class-mappngs
+	   (quote ,(apply #'compute-class-mappings class-mappings))
+	   :superclass-mappings
+	   (quote ,(apply #'compute-superclass-mappings class-mappings))
+	   :value-mappings
+	   (quote ,(apply #'compute-value-mappings class-mappings)))))
+
+;;      (quote ,(mapcar #'(lambda (class-mapping)
+;;			   (apply #'compute-mapping
+;;				  class-mapping nil class-mappings))
+;;		       class-mappings)))))
 
 ;; (defvar *mapping-schema-definitions* (make-hash-table))
 
